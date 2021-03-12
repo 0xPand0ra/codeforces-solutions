903361083bfa883d2b7dbe8405f54eea940b1525
HueOne <68436623+HueOne@users.noreply.github.com>
2021-03-12 22:35:10 +0100

    Add files via upload

---
+++ 228A_is_your_horseshoe_on_the_other_hoof.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <set>
+//#include <cstdio>
+using namespace std;
+int main(){
+	set<int> a;
+	int s;
+	for(int i=0;i<4;++i){
+		scanf("%d", &s);
+		a.insert(s);
+	}
+	cout << 4 - a.size();
+
+	return 0;
+}
