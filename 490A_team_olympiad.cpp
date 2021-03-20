9ec78b0b266949d9fa62f071974358cae215b0f8
HueOne <matigluchowski0410@gmail.com>
2021-03-20 22:21:01 +0100

    Add files via upload

---
+++ 490A_team_olympiad.cpp
@@ -0,0 +1,25 @@
+/*
+ * author: https://github.com/HueOne
+ * time = 31 ms
+ * GNU c++17
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int n, a, b=1, mi;
+	vector<int> v, v1, v2, v3;
+	scanf("%d", &n);
+	for(int i=0;i<n;++i)
+		scanf("%d", &a), v.push_back(a);
+	//sort(v.begin(), v.end());
+	for(int i=0;i<n;++i){
+		 if(v[i] == 1) v1.push_back(i+1);
+		 else if(v[i] == 2)v2.push_back(i+1);
+		 else v3.push_back(i+1);
+	}
+	mi = min(v1.size(),min(v2.size(), v3.size()));
+	printf("%d\n", mi);
+	for(int i=0;i<mi;++i)
+		printf("%d %d %d\n", v1[i], v2[i], v3[i]);
+	return 0;
+}
