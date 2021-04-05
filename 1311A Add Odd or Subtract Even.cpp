b19001750ea5b0107996205e60478d4215cf5d3f
HueOne <matigluchowski0410@gmail.com>
2021-04-05 23:26:45 +0200

    Update 1311A Add Odd or Subtract Even.cpp

--- 1311A Add Odd or Subtract Even.cpp
+++ 1311A Add Odd or Subtract Even.cpp
@@ -7,8 +7,6 @@ int main(){
 	cin >> t;
 	for(int i=0;i<t;++i){
 		cin >> a >>b;
-		//odd = nieparzysta
-		// even = parzysta
 		if(a-b > 0) cout << 1 + (a-b)%2<<endl;
 		else if(b-a > 0) (b-a)%2==0 ? cout << 2 <<endl : cout << 1 << endl;
 		else cout << 0 << endl;
