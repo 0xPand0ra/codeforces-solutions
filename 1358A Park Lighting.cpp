520d3a4e20e37e60b76622dac33d4c8a57d8281c
HueOne <matigluchowski0410@gmail.com>
2021-04-05 22:56:46 +0200

    Add files via upload

---
+++ 1358A Park Lighting.cpp
@@ -0,0 +1,18 @@
+/*
+ * author: https://github.com/HueOne
+ * time: 31 ms
+ * GNU c++17
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t, a, b;
+	cin >> t;
+	for(int i=0;i<t;++i){
+		scanf("%d %d", &a, &b);
+		a%2==0 ? printf("%d\n",a/2*b) : printf("%d\n", a/2*b + b/2 + b%2);
+	}
+
+	return 0;
+}
