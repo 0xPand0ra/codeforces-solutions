52930c409133907f6174c11d424077894774a400
HueOne <tonyhvhcsgo@gmail.com>
2021-05-26 13:17:31 +0200

    Add files via upload

---
+++ 630A - Again Twenty Five.cpp
@@ -0,0 +1,13 @@
+/*
+  time = 15 ms
+  author = https://github.com/HueOne
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  scanf("%d", &n);
+  printf("25\n");
+return 0;
+}
