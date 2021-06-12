07424491f8ce75d71b7b63e18ba0bbd799d3d5fb
HueOne <tonyhvhcsgo@gmail.com>
2021-06-12 22:51:35 +0200

    Create 1372A - Omkar and Completion.cpp

---
+++ 1372A - Omkar and Completion.cpp
@@ -0,0 +1,19 @@
+/*
+  time = 46 ms
+  author: https://github.com/HueOne
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t, n;
+  scanf("%d", &t);
+  for(int i=0; i<t; ++i){
+    scanf("%d", &n);
+    for(int j=0; j<n; ++j){
+      printf("%d ", 1);
+    }
+    printf("\n");
+  }
+}
