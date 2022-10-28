f445891eae9083a27da77c78123e736fbfdf84d0
Mateusz Głuchowski <matigluchowski0410@gmail.com>
2022-10-28 17:09:14 +0200

    Add file via upload

---
+++ python/69A - Young Physicist.py
@@ -0,0 +1,26 @@
+'''
+69A - Young Physicist
+https://github.com/0xPan0ra
+time:92 ms	
+memory:0 KB
+'''
+
+def func(vectors):
+    for i in range(3):
+        suma = 0
+        for j in range(len(vectors)):
+            suma += vectors[j][i]
+        if suma !=0 :
+            print('NO')
+            return
+    print('YES')
+
+if __name__ == '__main__':
+    ite = int(input())
+    vectors = []
+    for i in range(ite):
+        a, b, c = input().split(sep=' ')
+        vector = [int(a), int(b), int(c)]
+        vectors.append(vector)
+    
+    func(vectors)
